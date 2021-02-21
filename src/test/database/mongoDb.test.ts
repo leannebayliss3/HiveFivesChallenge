e4e41460f7745db290b19c93cc2d1a9bb9c723d7
Leanne Bayliss <leanne.bayliss3@gmail.com>
2021-02-21 12:33:22 +0000

    create basic db connection

---
+++ src/test/database/mongoDb.test.ts
@@ -0,0 +1,49 @@
+import {MongoDb} from "../../database/mongoDb";
+import {Connection} from "mongoose";
+import dotenv from "dotenv";
+
+describe('MongoDb connection', () => {
+
+    describe('getInstance', () => {
+        test('should exist', () => {
+            expect(MongoDb.getInstance).toBeDefined();
+        });
+        test('should return a Promise of a Connection', () => {
+            dotenv.config();
+
+            const mongoInstance = MongoDb.getInstance();
+            mongoInstance
+                .then((connection: any) => {
+                    expect(mongoInstance).toBeInstanceOf(Promise);
+                    expect(connection).toBeInstanceOf(Connection);
+                }).catch(error => {
+                throw Error(`getInstance - Promise of Connection: ${error}`);
+            })
+        });
+        test('should return the same object when called multiple times', () => {
+            dotenv.config();
+
+            jest.spyOn(MongoDb, 'getInstance');
+
+            const mongoInstance = MongoDb.getInstance();
+            const mongoInstance2 = MongoDb.getInstance();
+
+            mongoInstance.then(() => {
+                expect(MongoDb.getInstance).toHaveBeenCalledTimes(2);
+                expect(mongoInstance).toEqual(mongoInstance2);
+            }).catch(error => {
+                throw Error(`getInstance - same object test: ${error}`);
+            })
+        });
+        test('should throw an error when no database config is found', () => {
+            const mongoInstance = MongoDb.getInstance();
+            mongoInstance
+                .then((connection: any) => {
+                    throw Error(`getInstance - no db config: connection successfully established`);
+                }).catch(error => {
+                    expect(error).toEqual('DB_URI or PASSWORD cannot be undefined');
+            })
+        })
+    })
+
+});
