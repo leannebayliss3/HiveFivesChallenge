1c65967a6a888692465c5b60c3d585ff05b9ed5b
Leanne Bayliss <leanne.bayliss3@gmail.com>
2021-02-22 20:38:30 +0000

    refactor to use type ObjectTypeComposer

--- src/models/MongooseModel.ts
+++ src/models/MongooseModel.ts
@@ -1,46 +1,16 @@
 import mongoose from 'mongoose';
-import {composeMongoose} from "graphql-compose-mongoose";
+import {composeWithMongoose} from "graphql-compose-mongoose";
 import {ObjectTypeComposer} from "graphql-compose";
-import {GenerateResolverType} from "graphql-compose-mongoose/lib/composeMongoose";
 
 export class MongooseModel {
     readonly model: mongoose.Model<any>;
-    readonly modelTC: ObjectTypeComposer & {
-        mongooseResolvers: GenerateResolverType<any>;
-    };
+    readonly modelTC: ObjectTypeComposer;
 
-    private schema: mongoose.Schema | undefined = undefined;
+    private schema: mongoose.Schema;
 
     constructor(options: { modelName: string, modelSchema: {}, modelTCOpts: {} }) {
-        this.schema = new mongoose.Schema(options.modelSchema);
+        this.schema = new mongoose.Schema(options.modelSchema, {timestamps: true});
         this.model = mongoose.model(options.modelName, this.schema);
-        this.modelTC = composeMongoose(this.model, options.modelTCOpts);
-        this.modelTC.mongooseResolvers.createOne();
+        this.modelTC = composeWithMongoose(this.model, options.modelTCOpts);
     }
-
 }
-
-
-// import {composeWithMongoose, ComposeWithMongooseOpts} from 'graphql-compose-mongoose';
-// import {ObjectTypeComposer} from "graphql-compose";
-// import {Model} from "./Model";
-//
-// class recognitionModel implements Model {
-//     readonly Model: Model<any>;
-//     readonly ModelTC: ObjectTypeComposer;
-//
-//     constructor(opts: { modelName: string, modelSchema: Schema<any, any> }) {
-//         this.Model = this.createModel(opts.modelName, opts.modelSchema)
-//     }
-//
-//     get model() {
-//         return this.Model;
-//     }
-//
-//
-//
-//     private createModel(modelName: string, modelSchema: Schema<any, any>): Model<any> {
-//         return mongoose.model(modelName, modelSchema);
-//     }modelSchema
-//
-// }
