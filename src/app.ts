ad8b706831de9444525e868c17113339a05a3aeb
Leanne Bayliss <leanne.bayliss3@gmail.com>
2021-02-22 11:13:06 +0000

    create new graphql server

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
-import {MongoDb} from "./database/mongoDb";
 import dotenv from "dotenv";
 import express from 'express';
-import {Connection} from "mongoose";
+import * as expressGraphQl from 'express-graphql';
+
+import {Recognition} from './Schema/recognition';
+import {MongoDb} from "./database/mongoDb";
 
 if (process.env.NODE_ENV === 'development') {
     dotenv.config();
@@ -11,12 +13,14 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 MongoDb.getInstance()
-    .then((connection: Connection) => {
-        connection.on('error', console.log)
-            .on('open', (): void => {
-                app.listen(port,
-                    (): void => console.log(`Express server running on ${port}`));
-            })
+    .then(() => {
+        app.use('/graphql', expressGraphQl.graphqlHTTP({
+            schema: new Recognition().recognitionSchema,
+            graphiql: true
+        }))
+        app.listen(port,
+            (): void => console.log(`Express server running on ${port}`)
+        );
     })
     .catch((error: Error): void => {
         throw error;
