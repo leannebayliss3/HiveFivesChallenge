c42709878aab220a1308482e16da28d1193741ed
Leanne Bayliss <leanne.bayliss3@gmail.com>
2021-02-24 15:14:09 +0000

    refactor mongodb unit tests

--- test/database/mongoDb.test.ts
+++ test/database/mongoDb.test.ts
@@ -1,49 +1,58 @@
-import {MongoDb} from "../../src/database/mongoDb";
-import {Connection} from "mongoose";
-import dotenv from "dotenv";
+import {MongoDb} from '../../src/database/mongoDb';
+import mongoose from 'mongoose';
+import Spy = jasmine.Spy;
 
-describe('MongoDb connection', () => {
+describe('mongoDb', () => {
 
     describe('getInstance', () => {
+        let getInstanceSpy: Spy;
+
+        beforeEach(() => {
+            getInstanceSpy = spyOn(MongoDb, 'getInstance').and.callThrough();
+        });
+
+        afterEach(() => {
+            jest.clearAllMocks();
+        });
+
         test('should exist', () => {
             expect(MongoDb.getInstance).toBeDefined();
         });
-        test('should return a Promise of a Connection', () => {
-            dotenv.config();
 
-            const mongoInstance = MongoDb.getInstance();
-            mongoInstance
-                .then((connection: any) => {
-                    expect(mongoInstance).toBeInstanceOf(Promise);
-                    expect(connection).toBeInstanceOf(Connection);
-                }).catch(error => {
-                throw Error(`getInstance - Promise of Connection: ${error}`);
-            })
+        test('should call connect with db url and options', () => {
+            process.env.DB_URI = 'mock-db-url:<password>';
+            process.env.PASSWORD = '123456';
+
+            const connectSpy = spyOn(mongoose, 'connect').and.returnValue(Promise.resolve());
+            MongoDb.getInstance();
+
+            expect(connectSpy.calls.argsFor(0)).toEqual(['mock-db-url:123456', {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            }]);
+
         });
         test('should return the same object when called multiple times', () => {
-            dotenv.config();
-
-            jest.spyOn(MongoDb, 'getInstance');
+            process.env.DB_URI = 'mock-db-url:<password>';
+            process.env.PASSWORD = '123456';
+            spyOn(mongoose, 'connect');
 
             const mongoInstance = MongoDb.getInstance();
             const mongoInstance2 = MongoDb.getInstance();
 
-            mongoInstance.then(() => {
-                expect(MongoDb.getInstance).toHaveBeenCalledTimes(2);
-                expect(mongoInstance).toEqual(mongoInstance2);
-            }).catch(error => {
-                throw Error(`getInstance - same object test: ${error}`);
-            })
+            expect(getInstanceSpy).toHaveBeenCalledTimes(2);
+            expect(mongoInstance).toStrictEqual(mongoInstance2);
+
         });
         test('should throw an error when no database config is found', () => {
-            const mongoInstance = MongoDb.getInstance();
-            mongoInstance
-                .then(() => {
-                    throw Error(`getInstance - no db config: connection successfully established`);
-                }).catch(error => {
-                    expect(error).toEqual('DB_URI or PASSWORD cannot be undefined');
-            })
-        })
-    })
+            const connectSpy = spyOn(mongoose, 'connect');
+            try {
+                MongoDb.getInstance();
+            } catch (e) {
+                expect(connectSpy).not.toHaveBeenCalled();
+                expect(e).toEqual(new TypeError('DB_URI or PASSWORD cannot be undefined'));
+            }
+        });
+    });
 
 });
